b712cd3d7cf0aa2f68c1aef89e0e0fef6cabceb6
Alexander Gould <alexander.gould94@gmail.com>
2021-02-07 11:28:52 -0800

    commit tree c8eacb66dc9e0aa5de8bd6fdb688bd3744cb1295

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    people=[]
+    names.each do |person|
+        people.push "Hello, my name is #{person}."
+    end
+return people
+end
+
+def assign_rooms(speakers)
+    room_assignments = speakers.collect.with_index(1) do |speaker, index|
+         "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    end
+    return room_assignments
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |attendee|
+        puts "#{attendee}"
+    end
+    assign_rooms(attendees).each do |assignment|
+        puts "#{assignment}"
+    end
+end 
